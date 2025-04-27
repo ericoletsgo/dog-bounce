84087957b871f2eef707990c374d01caaa4037a5
Eric <ericnie12310@gmail.com>
2025-04-27 13:12:00 -0400

    drag/drop mech + throwing

--- js/dog.js
+++ js/dog.js
@@ -1,5 +1,4 @@
 import { distance } from './utils.js';
-import { USE_IMAGES } from './config.js';
 
 export class Dog {
   constructor(x, y, vx, vy, radius) {
@@ -8,16 +7,30 @@ export class Dog {
     this.vx = vx;
     this.vy = vy;
     this.radius = radius;
+    this.isDragging = false;
+  }
+
+  startDrag() {
+    this.isDragging = true;
+  }
+
+  stopDrag(throwVx, throwVy) {
+    this.isDragging = false;
+    this.vx = throwVx;
+    this.vy = throwVy;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
   update(canvasW, canvasH, gravity, restitution) {
-    // gravity
+    if (this.isDragging) return;
     this.vy += gravity;
-    // move
     this.x += this.vx;
     this.y += this.vy;
-
-    // bounce off walls
+    // bounce
     if (this.x - this.radius < 0) {
       this.x = this.radius;
       this.vx *= -restitution;
@@ -33,12 +46,11 @@ export class Dog {
     if (this.y + this.radius > canvasH) {
       this.y = canvasH - this.radius;
       this.vy *= -restitution;
-      this.vx *= 0.98; // friction on floor
+      this.vx *= 0.98;
     }
   }
 
   draw(ctx) {
-    // fallback circle only
     ctx.fillStyle = 'brown';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 import { setupCanvas } from './canvas.js';
 import { Dog }         from './dog.js';
 import { randomBetween } from './utils.js';
-import { GRAVITY, RESTITUTION, NUM_DOGS, DOG_RADIUS } from './config.js';
+import { GRAVITY, RESTITUTION, NUM_DOGS, DOG_RADIUS, DRAG_THROW_MULTIPLIER } from './config.js';
 
 let dogs = [];
-let ctx, canvas, width, height;
+let draggedDog = null;
+let dragOffsetX = 0, dragOffsetY = 0;
+let lastMouseX = 0, lastMouseY = 0;
+let throwVx = 0, throwVy = 0;
+
+const { canvas, ctx, width, height } = setupCanvas({
+  width: window.innerWidth * 0.9,
+  height: window.innerHeight * 0.9,
+  attachNode: '#canvas-container'
+});
 
 function createDogs() {
   dogs = [];
@@ -17,24 +26,57 @@ function createDogs() {
   }
 }
 
+function getMousePos(e) {
+  const rect = canvas.getBoundingClientRect();
+  const scale = window.devicePixelRatio || 1;
+  const x = (e.clientX - rect.left) * (canvas.width/rect.width) / scale;
+  const y = (e.clientY - rect.top) * (canvas.height/rect.height) / scale;
+  return { x, y };
+}
+
+function handleMouseDown(e) {
+  const pos = getMousePos(e);
+  lastMouseX = pos.x;
+  lastMouseY = pos.y;
+  throwVx = throwVy = 0;
+  for (let i = dogs.length - 1; i >= 0; i--) {
+    if (dogs[i].isPointInside(pos.x, pos.y)) {
+      draggedDog = dogs[i];
+      dragOffsetX = pos.x - draggedDog.x;
+      dragOffsetY = pos.y - draggedDog.y;
+      draggedDog.startDrag();
+      dogs.push(dogs.splice(i,1)[0]);
+      canvas.addEventListener('mousemove', handleMouseMove);
+      canvas.addEventListener('mouseup',   handleMouseUp);
+      break;
+    }
+  }
+}
+
+function handleMouseMove(e) {
+  if (!draggedDog) return;
+  const pos = getMousePos(e);
+  throwVx = (pos.x - lastMouseX) * DRAG_THROW_MULTIPLIER;
+  throwVy = (pos.y - lastMouseY) * DRAG_THROW_MULTIPLIER;
+  lastMouseX = pos.x;
+  lastMouseY = pos.y;
+  draggedDog.setPosition(pos.x - dragOffsetX, pos.y - dragOffsetY);
+}
+
+function handleMouseUp() {
+  if (!draggedDog) return;
+  draggedDog.stopDrag(throwVx, throwVy);
+  draggedDog = null;
+  canvas.removeEventListener('mousemove', handleMouseMove);
+  canvas.removeEventListener('mouseup',   handleMouseUp);
+}
+
 function gameLoop() {
-  ctx.clearRect(0, 0, width, height);
-  dogs.forEach(dog => {
-    dog.update(width, height, GRAVITY, RESTITUTION);
-    dog.draw(ctx);
-  });
+  ctx.clearRect(0,0,width,height);
+  dogs.forEach(d => { d.update(width,height, GRAVITY, RESTITUTION); d.draw(ctx); });
   requestAnimationFrame(gameLoop);
 }
 
-function init() {
-  ({ canvas, ctx, width, height } = setupCanvas({
-    width: window.innerWidth * 0.9,
-    height: window.innerHeight * 0.9,
-    attachNode: '#canvas-container'
-  }));
-
-  createDogs();
-  requestAnimationFrame(gameLoop);
-}
-
-init();
+createDogs();
+canvas.addEventListener('mousedown', handleMouseDown);
+requestAnimationFrame(gameLoop);
